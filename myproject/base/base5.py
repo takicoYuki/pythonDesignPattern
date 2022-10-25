70ad870fa73e4df85283e8dfbc9add66d013ef36
takita <takita.cons@gmail.com>
2022-10-26 01:32:13 +0900

    追加

---
+++ myproject/base/base5.py
@@ -0,0 +1,19 @@
+#2進数 8進数 16進数
+
+from re import A
+
+#2進数 ob
+age = 0b111 #7
+print(age)
+
+#8進数 0o
+age = 0o11 #9
+print(age)
+
+#16進数 0x
+age = 0x11 #17
+
+#2進数 => 文字列
+print(bin(15)) #0b1111
+print(oct(15)) #0o17
+print(hex(15)) #0xf
